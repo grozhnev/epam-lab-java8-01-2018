2b68664c8e0b75948222f019a32584428305cde4
Ivan Seliukov <Ivan_Seliukov@epam.com>
2018-01-27 12:34:00 +0300

    Change methods to Functions

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -15,25 +16,20 @@ import static org.junit.Assert.assertEquals;
 @SuppressWarnings({"WeakerAccess", "unused"})
 public class Exercise1 {
 
-    private static Person personExtractor(Employee employee) {
-        return employee.getPerson();
-    }
-
-    private static String fullNameExtractor(Person person) {
-        return person.getFullName();
-    }
-
-    private static int fullNameLengthExtractor(String fullName) {
-        return fullName.length();
-    }
-
     @Test
     public void mapEmployeesToLengthOfTheirFullNames() {
         List<Employee> employees = Example1.getEmployees();
         List<Integer> lengths = new ArrayList<>();
 
+        Function<Employee, Person> personExtractor = Employee::getPerson;
+        Function<Person, String> fullNameExtractor = Person::getFullName;
+        Function<String, Integer> fullNameLengthExtractor = String::length;
+
+        Function<Employee, Integer> employeeToLengthOfHisFullName =
+                e -> fullNameLengthExtractor.apply(fullNameExtractor.apply(personExtractor.apply(e)));
+
         for (Employee employee : employees) {
-            lengths.add(fullNameLengthExtractor(fullNameExtractor(personExtractor(employee))));
+            lengths.add(employeeToLengthOfHisFullName.apply(employee));
         }
 
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
